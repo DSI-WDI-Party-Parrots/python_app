a6bcbb8645f65e299be788af0bdd2a81b4fac9d7
kgracia44 <kgracia44@gmail.com>
2016-08-08 23:59:40 -0400

    dynamically set port based on Heroku environment

--- airbnb_app.py
+++ airbnb_app.py
@@ -125,4 +125,8 @@ if __name__ == '__main__':
 
     # app.run(HOST,PORT)
     # app.run(debug=True)
-    app.run()
+
+    # For Heroku
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
